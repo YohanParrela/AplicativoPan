3a55da378b5cb80e8e6276f3b746215ca152375b
pcetec <pc@etec.com>
2026-09-16 20:31:37 -0300

    commitando

--- app/src/main/java/com/example/aplicativopan/MainActivity.kt
+++ app/src/main/java/com/example/aplicativopan/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.aplicativopan
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,12 +11,20 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.aulaid)
+
+
+        val meuBotao = findViewById<Button>(R.id.btnProxima)
+        meuBotao.text = "Novo texto"
+
+        meuBotao.setOnClickListener {
+            if (meuBotao.text == "Novo texto"){
+                meuBotao.text = "Cliquei"
+            }else{
+                meuBotao.text = "Cliquei de novo"
+            }
         }
+
+
     }
 }
